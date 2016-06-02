7ef7082460b6852a7ccc64906803b0585d69ac23
Dirk Su <xanthein310@gmail.com>
2016-06-02 16:30:55 +0800

    modify build.sh

--- build.sh
+++ build.sh
@@ -2,11 +2,13 @@
 
 mkdir release
 
+echo "### 64bit version ###"
 ./configure
 make
 cp src/ipmitool release/ipmitool
 make clean
 
+echo "### 32bit version ###"
 ./configure --build=i686-pc-linux-gnu "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32"
 make
 cp src/ipmitool release/ipmitool_32
