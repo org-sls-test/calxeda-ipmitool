8b1054fa2f4c151211357d41a9ab832c05c5fcac
Dirk Su <xanthein310@gmail.com>
2016-06-02 17:51:26 +0800

    change build sequence

--- build.sh
+++ build.sh
@@ -3,12 +3,15 @@
 mkdir release
 
 echo "### 64bit version ###"
+sudo apt-get install libssl-dev
 ./configure
 make
 cp src/ipmitool release/ipmitool
 make clean
+sudo apt-get remove libssl-dev
 
 echo "### 32bit version ###"
+sudo apt-get install libssl-dev:i386 gcc-multilib
 ./configure --build=i686-pc-linux-gnu "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32"
 make
 cp src/ipmitool release/ipmitool_32
